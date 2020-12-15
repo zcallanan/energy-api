a94d0a990042194e0ab3600826f7c056a3da64bf
Zach Callanan <caveofmonsters@gmail.com>
2020-12-15 21:42:40 +0100

    configuration

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn de-energy.wsgi
--- django_energy_api/django_energy_api/settings.py
+++ django_energy_api/django_energy_api/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'kf5%d+92ghnkg$+s(ty8h9u4$^*fy_ppu393vx9n0um)2$+di&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['de-energy.herokuapp.com']
 
 
 # Application definition
@@ -75,15 +77,18 @@ WSGI_APPLICATION = 'django_energy_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'energy',
-        'USER': 'zach',
-        'PASSWORD': '@nnie',
-        'HOST': ''
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'energy',
+#         'USER': 'zach',
+#         'PASSWORD': '@nnie',
+#         'HOST': ''
+#     }
+# }
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 
 
 # Password validation
@@ -123,3 +128,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
