4392c50dc5a16688266475ea8b4407395648cce1
Zach Callanan <caveofmonsters@gmail.com>
2021-01-22 20:10:14 +0100

    added environ to installed_apps

--- django_energy_api/django_energy_api/settings/common.py
+++ django_energy_api/django_energy_api/settings/common.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'energyapp',
-    'django_filters'
+    'django_filters',
+    'environ'
 ]
 
 MIDDLEWARE = [
